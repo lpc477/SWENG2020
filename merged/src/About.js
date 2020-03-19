49262f80b0bc6891eb7f5cd10969c09c9d7f4e03
james rowland <rowlanja@tcd.ie>
2020-03-19 22:17:47 +0000

    UPDATED MERGED COPY OF ME AND STEPHANS WORK WITH SIMPLIFIED BUT WORKING UI TO DO : PUT IN SAMPLE CONVERSATIONS PICTURES AND MAYBE REWORK UI

---
+++ merged/src/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const About = () => (
+  <div>
+      <h2 class="display-3">About Us!</h2>
+      
+      <br/>
+      <p class="display-5">Nothin really....</p>
+      <p class="display-5">Thank You!</p>
+      <br/>
+      <br/>
+  </div>
+)
---
+++ merged/src/Home.js
@@ -0,0 +1,69 @@
+import React from 'react'
+
+export const Home = () => (
+  <div>
+     <header class="masthead text-center text-white">
+    <div class="masthead-content">
+      <div class="container">
+        <a href="App1" class="btn btn-primary btn-xl rounded-pill mt-5">Begin Chat</a>
+      </div>
+    </div>
+
+  </header>
+
+  <section>
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-6 order-lg-2">
+          <div class="p-5">
+            <img class="img-fluid rounded-circle" src="img/01.jpg" alt="" />
+          </div>
+        </div>
+        <div class="col-lg-6 order-lg-1">
+          <div class="p-5">
+            <h2 class="display-4">insert title</h2>
+            <p>nsert text, not finished as no pictures yet pictures will go in empty space</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-6">
+          <div class="p-5">
+            <img class="img-fluid rounded-circle" src="img/02.jpg" alt="" />
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="p-5">
+            <h2 class="display-4">insert title</h2>
+            <p>nsert text, not finished as no pictures yet</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-6 order-lg-2">
+          <div class="p-5">
+            <img class="img-fluid rounded-circle" src="img/03.jpg" alt="" />
+          </div>
+        </div>
+        <div class="col-lg-6 order-lg-1">
+          <div class="p-5">
+            <h2 class="display-4">nsert text</h2>
+            <p>nsert text, , not finished as no pictures yet</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+</div>
+)
