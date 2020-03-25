fd7b2ee1e32609fd94181094617ef1fdf071afa1
steviejeebies <rowes@tcd.ie>
2020-03-25 08:41:42 +0000

    restored the 'bot' folder

---
+++ bot/translationSettings.js
@@ -0,0 +1,11 @@
+const TranslationSettings = {
+    englishEnglish: 'en',
+    englishSpanish: 'es',
+    spanishEnglish: 'in',
+    spanishSpanish: 'it',
+    englishDutch: 'nl',
+    dutchEnglish: 'en',
+    dutchDutch: 'nl'
+};
+
+module.exports.TranslationSettings = TranslationSettings;
